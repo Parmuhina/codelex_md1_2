8e8ad391d93e19ab02225725c0ce7f04434d9cca
Parmuhina <60193114+Parmuhina@users.noreply.github.com>
2022-10-31 09:25:37 +0200

    Add files via upload

---
+++ md1/array-objects/1.php
@@ -0,0 +1,4 @@
+<?php
+$intArray=array(4, 6, 8);
+foreach ($intArray as $int){$sum+=$int;};
+echo "$sum".PHP_EOL;
---
+++ md1/array-objects/3.php
@@ -0,0 +1,9 @@
+<?php
+$person=new stdClass();
+$person->name="John";
+$person->surname="Doe";
+$person->age=50;
+
+var_dump($person->age);
+var_dump($person->surname);
+var_dump($person->name);
